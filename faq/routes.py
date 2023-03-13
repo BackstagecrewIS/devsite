c985ec5a7678d1a698b08bc76cac2100669d07ea
BackstagecrewIS <locksmith@backstagecrew.com>
2023-03-13 01:30:40 +0000

    Add styling to faq.html

--- faq/routes.py
+++ faq/routes.py
@@ -6,3 +6,13 @@ from faq.models import Category, Question
 @app.route("/")
 def home():
     return render_template("faq.html")
+
+
+@app.route("/categories")
+def categories():
+    return render_template("categories.html")
+
+
+@app.route("/add_category", methods=["GET", "POST"])
+def add_category():
+    return render_template("add_category.html")
