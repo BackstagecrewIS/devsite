58c16fb68bb8868469e31fc80296a086662a20d3
BackstagecrewIS <locksmith@backstagecrew.com>
2023-03-26 15:43:02 +0000

    Adjust database configuration

--- faq/models.py
+++ faq/models.py
@@ -32,7 +32,7 @@ class Question(db.Model):
 class User(db.Model):
     # schema for the Users model
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.String(25), nullable=False)
     password = db.Column(db.String(210), nullable=False)
     email = db.Column(db.String(50), nullable=False)
 
